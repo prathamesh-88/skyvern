1400fc25ed0894df16147441d7e7e5a77f96c1ff
Piyush Mittal <20piyushmittal@gmail.com>
2025-01-10 15:54:17 +0530

    changes_in_ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
             repo: "git@github.com:prathamesh-88/skyvern.git",
             path: "/home/app/skyvern_pro",
             ssh_options: ["ForwardAgent=yes"],
-            "post-deploy": "cd /home/app/skyvern_pro && cp ~/.private/.env_skyvern ~/skyvern_pro/current/.env && pm2 reload ecosystem.config.js",
+            "post-deploy": "cd /home/app/skyvern_pro/current && cp ~/.private/.env_skyvern ~/skyvern_pro/current/.env && pm2 reload ecosystem.config.js",
             "post-setup": "python3 -m pip install --user pipx && python3 -m pipx ensurepath && pipx install poetry==1.7.1"
         }
     }
