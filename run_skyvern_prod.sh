bfdc677bf5873a9b99f947035900f664fba103c1
Prathamesh Tamanekar <prathameshtamanekar@gmail.com>
2025-01-14 17:27:27 +0530

    chore: update deploy script

--- run_skyvern_prod.sh
+++ run_skyvern_prod.sh
@@ -6,7 +6,8 @@ if [ ! -f .env ]; then
   cp .env.example .env
   echo "Please add your api keys to the .env file."
 fi
+export DISPLAY=:99
 source "$(poetry env info --path)/bin/activate"
 poetry install
 ./run_alembic_check.sh
-xvfb-run poetry run python3.11 -m skyvern.forge
+poetry run python3.11 -m skyvern.forge
